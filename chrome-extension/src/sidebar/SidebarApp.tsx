8b45ada323e25cbac2d34268d407520f0a8723b1
Rui Tu <this.ruitu@gmail.com>
2025-05-03 13:07:11 -0700

    init chrome extension

---
+++ chrome-extension/src/sidebar/SidebarApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SidebarApp: React.FC = () => {
+    return (
+        <div style={{ padding: '10px', fontFamily: 'sans-serif' }}>
+            <h1>LiveKit Sidebar</h1>
+            <p>React App Loaded!</p>
+            <p>Phase 1 Complete. LiveKit components will be added next.</p>
+            {/* LiveKit components will go here in Phase 2 */}
+        </div>
+    );
+};
+
+export default SidebarApp; 
